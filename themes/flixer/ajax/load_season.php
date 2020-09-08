42fba74c44d66a7cbdb9b795c483ddf4dd1500f4
Kringe59 <commercialhours2@gmail.com>
2020-09-08 22:39:48 +0600

    episode front end issue fixed

--- themes/flixer/ajax/load_season.php
+++ themes/flixer/ajax/load_season.php
@@ -10,22 +10,39 @@ $muviko->startUserSession($_SESSION);
 define('THEME_PATH', $core->getThemePath());
 define('UPLOADS_PATH', $core->getUploadsPath());
 $season_id = $_GET['season_id'];
-$seasons = $db->query("SELECT season_number FROM seasons WHERE id='".$season_id."'");
+$sesion_sql = "SELECT season_number, movie_id, A.session_img FROM seasons 
+left Join (SELECT id, movie_poster_image as session_img FROM movies ) as A ON seasons.movie_id = A.id 
+ WHERE seasons.id='".$season_id."'";
+$seasons = $db->query($sesion_sql);
 $season = $seasons->fetch_object();
-if($season->season_number < 10){ $sN = 'Season  0'.$season->season_number;}else{ $sN = 'Season '.$season->season_number;}
+
+if($season->season_number < 10)
+{ 
+	$sN = 'Season  0'.$season->season_number;
+}
+else
+{ 
+	$sN = 'Season '.$season->season_number;
+}
 $episodes = $db->query("SELECT * FROM episodes WHERE season_id='".$season_id."' ORDER by id ASC");
 $count = 1;
 while($episode = $episodes->fetch_object()) {
 	//echo strpos($episode->episode_thumbnail,"maxresdefault-1.jpg");
-	if(strpos($episode->episode_thumbnail,"maxresdefault-1.jpg") !== false){
-		 $eI = getposterImg($episode->movie_id);
-	}else{
-		 $eI = UPLOADS_PATH.'/episodes/'.$episode->episode_thumbnail ;
+	$imgtype  = @exif_imagetype($episode->episode_thumbnail);
+	if ($imgtype === false) 
+	{
+		$eI = UPLOADS_PATH.'/poster_images/'.$season->session_img ;
+	}
+	else
+	{
+
+		if(strpos($episode->episode_thumbnail,"maxresdefault-1.jpg") !== false){
+			 $eI = getposterImg($episode->movie_id);
+		}else{
+			 $eI = UPLOADS_PATH.'/episodes/'.$episode->episode_thumbnail ;
+		}
 	}
 $check = $db->query("SELECT * FROM my_watched_episodes WHERE episode_id='".$episode->id."' AND user_id='".$muviko->user->id."'");
-//$check = $db->query("SELECT * FROM my_watched WHERE movie_id='".$episode->id."' AND user_id='".$muviko->user->id."'");
-
-
 echo
 '
 <div class="episode col-md-4">';
@@ -38,8 +55,8 @@ echo '<a style="cursor:pointer;" onclick="loadEpisodepage('.$episode->id.'); ret
 	if($episode->episode_name!=''){
 		$epName = $episode->episode_name;
 	}
-	$newEpisode_name = '';
-	 if($count < 10)
+	$newEpisode_name = 'Episode '.$episode->season_sub_id;
+	/*  if($count < 10)
 	 {
 	     $newEpisode_name = 'Episode 0'.$count; 
 	     
@@ -48,7 +65,7 @@ echo '<a style="cursor:pointer;" onclick="loadEpisodepage('.$episode->id.'); ret
 	 {
 	     $newEpisode_name='Episode '.$count;
 	     
-	 }
+	 } */
 
 echo '<p class="title" title="'.$episode->episode_name.'" style="color:white;">  '.$sN.' | '.$newEpisode_name.' <br>'.$epName.' </p></a>
 <p class="description"> '.substr($episode->episode_description,0,100).' </p>
